7c400be12795ad5c4490acd99ecba643df4ed818
kiran316 <52204268+kiran316@users.noreply.github.com>
2024-04-15 15:31:21 +0200

    Create ecr.tf

---
+++ terraform-eks/ecr.tf
@@ -0,0 +1,31 @@
+resource "aws_ecr_repository" "app_repo" {
+  name                 = "hivemind_app_repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.app_repo.name
+  policy     = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 14 days",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 14
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
