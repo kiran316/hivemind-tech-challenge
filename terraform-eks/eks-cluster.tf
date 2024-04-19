a614598f3b806b6894229347daa207653bda5f5b
kiran316 <kiranvilkhu328@gmail.com>
2024-04-19 10:39:06 +0200

    terraform update

--- terraform-eks/eks-cluster.tf
+++ terraform-eks/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~>19.21.0"
+  version         = "20.8.5"
   cluster_name    = local.cluster_name
   cluster_version = var.kubernetes_version
 
@@ -9,27 +9,6 @@ module "eks" {
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
-
-  cluster_addons = [
-    {
-      addon_name        = "vpc-cni"
-      addon_version     = "v1.18.0-eksbuild.1"
-      cluster_name      = local.cluster_name
-      resolve_conflicts = "OVERWRITE"
-    },
-    {
-      addon_name        = "kube-proxy"
-      addon_version     = "v1.29.0-eksbuild.1"
-      cluster_name      = local.cluster_name
-      resolve_conflicts = "OVERWRITE"
-    },
-    {
-      addon_name        = "coredns"
-      addon_version     = "v1.11.1-eksbuild.4"
-      cluster_name      = local.cluster_name
-      resolve_conflicts = "OVERWRITE"
-    }
-  ]
 
   enable_irsa = true
 
